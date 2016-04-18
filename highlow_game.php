f5d7db7605d0a9b139704264fd0b6af0cbf4aba8
Karina <karina@alisons-MacBook-Air.local>
2016-04-18 10:28:09 -0500

    Added the ability for the user to input their own min and max numbers.

--- highlow_game.php
+++ highlow_game.php
@@ -1,11 +1,14 @@
 <?php
 
-var_dump($argc);
-var_dump ($argv);
-
 function game() {
-    //game picks a random number between 1 and 100
-    $random = mt_rand (1, 100);
+    
+    global $argc, $argv;
+    
+    if ($argc == 3) {
+        $random = mt_rand ($argv[1], $argv[2]);
+    } else if ($argc == 1) {
+        $random = mt_rand (1, 100);
+    }
 
     fwrite (STDOUT, "Can you guess what number I'm thinking of?\n");
     $guess = fgets(STDIN);
@@ -16,7 +19,7 @@ function game() {
         $tries++;
         if ($guess < $random) {
             fwrite (STDOUT, "higher! keep going:");
-            $guess = fgets(STDIN) . PHP_EOL;  
+            $guess = fgets(STDIN);  
         } else if ($guess > $random) {
             fwrite (STDOUT, "lower! keep going:");
             $guess = fgets(STDIN);
@@ -27,7 +30,9 @@ function game() {
     again();
 } 
 
+
 function again() {
+    
     fwrite (STDOUT, "Play? Type 1 for yes, 2 for no\n");
 
     $maybe = fgets(STDIN);
